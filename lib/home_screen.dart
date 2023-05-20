d38e80f0e60c5c2307cbe68c8b825ceaa076dc8b
yobra254 <brayodhis071@gmail.com>
2023-05-21 01:29:49 +0300

    UI and Attaching to CRUD Functions

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -25,9 +25,6 @@ class _HomeScreenState extends State<HomeScreen> {
     _refreshData();
   }
 
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descController = TextEditingController();
-
   Future<void> _addData() async {
     await SQLHelper.createData(_titleController.text, _descController.text);
     _refreshData();
@@ -46,10 +43,147 @@ class _HomeScreenState extends State<HomeScreen> {
         content: Text('Data Deleted'),
       ),
     );
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  void showBottomSheet(int? id) async {
+    //if id is not null then it will update otherwise it will be added
+    //when edit button is pressed id will be given to bottomsheet function and edited
+    if (id != null) {
+      final existingData =
+          _allData.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+
+    showModalBottomSheet(
+      elevation: 5,
+      isScrollControlled: true,
+      context: context,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 40.0,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 50,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Title",
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _descController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Description",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (id == null) {
+                    await _addData();
+                  }
+                  if (id != null) {
+                    await _updateData(id);
+                  }
+
+                  _titleController.text = "";
+                  _descController.text = "";
+//Hide Bottom Sheet
+                  Navigator.of(context).pop();
+                  print("Data Added");
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(18),
+                  child: Text(
+                    id == null ? "Add Data" : "Update",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFECEAF4),
+      appBar: AppBar(
+        title: Text(
+          "CRUD Operations",
+        ),
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allData.length,
+              itemBuilder: (context, index) => Card(
+                margin: EdgeInsets.all(15),
+                child: ListTile(
+                  title: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      _allData[index]['title'],
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  subtitle: Text(_allData[index]['desc']),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          showBottomSheet(_allData[index]['id']);
+                        },
+                        icon: Icon(
+                          Icons.edit_square,
+                          color: Colors.indigo,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _deleteData(_allData[index]['id']);
+                        },
+                        icon: Icon(
+                          Icons.delete_forever_rounded,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showBottomSheet(null),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
