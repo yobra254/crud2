01ba864826049fc3492c9fec6e6c4da381cb8a94
yobra254 <brayodhis071@gmail.com>
2023-05-20 23:31:08 +0300

    Creation of CRUD Functions

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,52 @@
+import 'package:crud2/db_helper.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   //const HomeScreen({super.key});
+  List<Map<String, dynamic>> _allData = [];
+  bool _isLoading = true;
+
+  void _refreshData() async {
+    final data = await SQLHelper.getAllData();
+    setState(() {
+      _allData = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  Future<void> _addData() async {
+    await SQLHelper.createData(_titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  Future<void> _updateData(int id) async {
+    await SQLHelper.updateData(id, _titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  void _deleteData(int id) async {
+    await SQLHelper.deleteData(id);
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        backgroundColor: Colors.redAccent,
+        content: Text('Data Deleted'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
